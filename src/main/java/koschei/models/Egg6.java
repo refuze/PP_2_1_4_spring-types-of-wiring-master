aa160ba521f4fc85a60d90234b1740d924045db7
refuze <refuzeapple@gmail.com>
2022-11-10 13:19:20 +0300

    Implemented a constructor dependency in the Egg class.

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    @Autowired
+
     private Needle7 needle7;
+
+    @Autowired
+    public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
 
     @Override
     public String toString() {
